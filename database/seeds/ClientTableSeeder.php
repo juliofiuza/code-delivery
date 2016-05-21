3576b496b10b6141e744c0fd1799ecb09fde001e
Julio Fiuza <julio.fiuza@aesirsolucoes.com.br>
2016-05-21 17:22:19 -0300

    Criando base do sistema

---
+++ database/seeds/ClientTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use CodeDelivery\Models\Client;
+use Illuminate\Database\Seeder;
+
+class ClientTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Client::class, 10)->create();
+    }
+}
