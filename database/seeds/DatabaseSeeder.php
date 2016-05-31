a49369715ab87428060fce895bee90d19af4c9da
Julio Fiuza <julio.fiuza@aesirsolucoes.com.br>
2016-05-31 08:55:34 -0300

    Fase 3 - Correção

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         //$this->call(ClientTableSeeder::class);
         $this->call(OrderTableSeeder::class);
         $this->call(CupomTableSeeder::class);
+        $this->call(OAuthTableSeeder::class);
 
         Model::reguard();
     }
